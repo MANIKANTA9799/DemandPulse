8fa388b0b82d9c68caa130994a91c7c5c86a06a4
Manikanta <mytab123yy@gmail.com>
2026-04-01 13:43:43 +0530

    exception and logging done and tested

--- demo.py
+++ demo.py
@@ -1,14 +1,14 @@
-from src.logger.logger import get_logger
-from src.exception.custom_exception import CustomException
-import sys
+# from src.logger.logger import get_logger
+# from src.exception.custom_exception import CustomException
+# import sys
 
-logger = get_logger(__name__)
+# logger = get_logger(__name__)
 
-try:
-    logger.info("starting data ingestion")
+# try:
+#     logger.info("starting data ingestion")
 
-    x = 10 / 0   # error
+#     x = 10 / 0   # error
 
-except Exception as e:
-    logger.error("something failed")
-    raise CustomException(e, sys)#type:ignore 
+# except Exception as e:
+#     logger.error("something failed")
+#     raise CustomException(e, sys)#type:ignore 
